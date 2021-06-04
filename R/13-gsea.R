9fbfe49f76c9de619857cb7c1115afe6ac832ce7
Mickaël Canouil <mickael.canouil@cnrs.fr>
2021-06-04 12:36:37 +0200

    comments plots (optional)

--- R/13-gsea.R
+++ R/13-gsea.R
@@ -64,7 +64,7 @@ theme_update(
 )
 
 
-### ene-set Enrichment =============================================================================
+### Gene-set Enrichment ============================================================================
 results_file <- here("outputs", "11-twas", paste0(project_name, "_DE_status_gene.csv.gz"))
 
 enrich_sets <- lapply(
@@ -214,164 +214,164 @@ enrich_sets <- lapply(
   }
 )
 
-invisible(lapply(
-  X = enrich_sets,
-  FUN = function(.enrich_sets) {
-    .enrich_sets <- .enrich_sets[!sapply(.enrich_sets, is.null)]
-
-    if (
-      all(
-        sapply(
-          X = .enrich_sets, 
-          FUN = function(.enrich) nrow(setDT(.enrich@result)[p.adjust < fdr_pathway])
-        ) == 0
-      )
-    ) {
-      return(NULL)
-    }
-    
-    plot_scales <- rbindlist(lapply(.enrich_sets, FUN = function(.enrich) {
-      setDT(.enrich@result)[
-        p.adjust < fdr_pathway
-      ][
-        j = list(
-          p.adjust = max(round(-log10(c(fdr_pathway, p.adjust)), digits = 0)),
-          setSize = max(c(setSize, 0))
-        )
-      ]
-    }))[j = lapply(.SD, max)] + 1
-    
-    enrich_plots <- lapply(.enrich_sets, FUN = function(.enrich) {
-      .dt <- setDT(.enrich@result)[
-        p.adjust < fdr_pathway
-      ][
-        order(enrichmentScore)
-      ][
-        j = Description := factor(Description, levels = unique(Description))
-      ]
-      
-      if (nrow(.dt) == 0) {
-        return(
-          ggplot() +
-            annotate(geom = "text", x = 1, y = 1, label = paste0("No enriched terms found\n(FDR < ", fdr_pathway, ").")) +
-            theme(
-              axis.text.x = element_blank(), 
-              axis.text.y = element_blank(), 
-              axis.title.x = element_blank(), 
-              axis.title.y = element_blank(),
-              panel.grid = element_blank()
-            )
-        )
-      }
-      
-      is_too_big <- nrow(.dt) > 25
-      
-      if (is_too_big) .dt <- head(.dt[order(pvalue)], 25)
-      
-      ggplot(data = .dt) +
-        aes(x = enrichmentScore, y = Description, colour = -log10(p.adjust), size = setSize) +
-        geom_point() +
-        scale_y_discrete(
-          labels = function(.x) {
-            .x <- ifelse(
-              nchar(.x) > 50,
-              gsub("(.{0,50}).*", "\\1 [...]", .x),
-              .x
-            )
-            gsub("(^|[[:space:]])([[:alpha:]])", "\\1\\U\\2", .x, perl = TRUE)
-          }
-        ) +
-        scale_colour_continuous(limits = c(-log10(fdr_pathway), plot_scales[["p.adjust"]])) +
-        scale_size_continuous(limits = c(1, plot_scales[["setSize"]]), range = c(0.1, 3)) +
-        labs(
-          x = if (is_too_big) "Enrichment Score (Top 25)" else "Enrichment Score", 
-          y = NULL, 
-          colour = "-log<sub>10</sub>(FDR)<br>(Benjamini-Hochberg)",
-          size = "Number of Genes"
-        ) +
-        theme_minimal(base_size = 10, base_family = "Tex Gyre Termes") +
-        theme(
-          plot.title.position = "plot",
-          plot.caption.position = "plot",
-          plot.title = element_markdown(),
-          plot.subtitle = element_markdown(face = "italic", size = rel(0.80)),
-          plot.caption = element_markdown(face = "italic", size = rel(0.75)),
-          axis.title.x = element_markdown(),
-          axis.text.x = element_markdown(),
-          axis.title.y = element_markdown(),
-          axis.text.y = element_markdown(size = rel(0.75)),
-          panel.grid.minor = element_blank(),
-          egend.title = element_markdown()
-        ) +
-        guides(
-          colour = guide_colourbar(
-            title.position = "top", 
-            title.hjust = 0.5, 
-            barwidth = unit(10, units = "char"), 
-            direction = "horizontal", 
-            order = 2
-          ),
-          size = guide_legend(
-            title.position = "top", 
-            title.hjust = 0.5, 
-            direction = "horizontal", 
-            order = 1
-          )
-        )
-    })
-    
-    caption_plots <- sapply(.enrich_sets, FUN = function(.enrich) {
-      sprintf(
-        fmt = "%s (p-value < %s)", 
-        format(length(.enrich@geneList), digits = 0, big.mark = ","), 
-        pvalue_gene
-      )
-    })
-    caption_plots <- sprintf(fmt = "<b>%s</b>) %s",
-      LETTERS[seq_along(caption_plots)],
-      caption_plots
-    )
-    
-    subtitle_plots <- sprintf(fmt = "<b>%s</b>) %s",
-      LETTERS[seq_along(enrich_plots)], 
-      names(enrich_plots)
-    )
-    
-    agg_png(
-      filename = file.path(output_directory, "gene_set_enrichment.png"),
-      width = 16, height = 16, units = "cm", res = 300, scaling = 0.60
-    )
-      print(
-        wrap_plots(c(enrich_plots, list(guide_area())), ncol = 2, guides = "collect") +
-          plot_annotation(
-            title = "Gene Set Enrichment Analysis",
-            subtitle = sprintf("With %s and %s.",
-              paste(subtitle_plots[-length(subtitle_plots)], collapse = ", "),
-              subtitle_plots[length(subtitle_plots)]
-            ),
-            caption = sprintf("Mapped genes for %s and %s.",
-              paste(caption_plots[-length(caption_plots)], collapse = ", "),
-              caption_plots[length(caption_plots)]
-            ),
-            tag_levels = "A",
-            theme = theme_minimal(base_size = 10, base_family = "Tex Gyre Termes") +
-              theme(
-                plot.title.position = "plot",
-                plot.caption.position = "plot",
-                plot.title = element_markdown(),
-                plot.subtitle = element_markdown(face = "italic", size = rel(0.80)),
-                plot.caption = element_markdown(face = "italic", size = rel(0.75)),
-                axis.title.x = element_markdown(),
-                axis.text.x = element_markdown(),
-                axis.title.y = element_markdown(),
-                axis.text.y = element_markdown(),
-                panel.grid.minor = element_blank()
-              )
-          )
-      )
-    invisible(dev.off())
-  }
-))
+# invisible(lapply(
+#   X = enrich_sets,
+#   FUN = function(.enrich_sets) {
+#     .enrich_sets <- .enrich_sets[!sapply(.enrich_sets, is.null)]
+# 
+#     if (
+#       all(
+#         sapply(
+#           X = .enrich_sets, 
+#           FUN = function(.enrich) nrow(setDT(.enrich@result)[p.adjust < fdr_pathway])
+#         ) == 0
+#       )
+#     ) {
+#       return(NULL)
+#     }
+#     
+#     plot_scales <- rbindlist(lapply(.enrich_sets, FUN = function(.enrich) {
+#       setDT(.enrich@result)[
+#         p.adjust < fdr_pathway
+#       ][
+#         j = list(
+#           p.adjust = max(round(-log10(c(fdr_pathway, p.adjust)), digits = 0)),
+#           setSize = max(c(setSize, 0))
+#         )
+#       ]
+#     }))[j = lapply(.SD, max)] + 1
+#     
+#     enrich_plots <- lapply(.enrich_sets, FUN = function(.enrich) {
+#       .dt <- setDT(.enrich@result)[
+#         p.adjust < fdr_pathway
+#       ][
+#         order(enrichmentScore)
+#       ][
+#         j = Description := factor(Description, levels = unique(Description))
+#       ]
+#       
+#       if (nrow(.dt) == 0) {
+#         return(
+#           ggplot() +
+#             annotate(geom = "text", x = 1, y = 1, label = paste0("No enriched terms found\n(FDR < ", fdr_pathway, ").")) +
+#             theme(
+#               axis.text.x = element_blank(), 
+#               axis.text.y = element_blank(), 
+#               axis.title.x = element_blank(), 
+#               axis.title.y = element_blank(),
+#               panel.grid = element_blank()
+#             )
+#         )
+#       }
+#       
+#       is_too_big <- nrow(.dt) > 25
+#       
+#       if (is_too_big) .dt <- head(.dt[order(pvalue)], 25)
+#       
+#       ggplot(data = .dt) +
+#         aes(x = enrichmentScore, y = Description, colour = -log10(p.adjust), size = setSize) +
+#         geom_point() +
+#         scale_y_discrete(
+#           labels = function(.x) {
+#             .x <- ifelse(
+#               nchar(.x) > 50,
+#               gsub("(.{0,50}).*", "\\1 [...]", .x),
+#               .x
+#             )
+#             gsub("(^|[[:space:]])([[:alpha:]])", "\\1\\U\\2", .x, perl = TRUE)
+#           }
+#         ) +
+#         scale_colour_continuous(limits = c(-log10(fdr_pathway), plot_scales[["p.adjust"]])) +
+#         scale_size_continuous(limits = c(1, plot_scales[["setSize"]]), range = c(0.1, 3)) +
+#         labs(
+#           x = if (is_too_big) "Enrichment Score (Top 25)" else "Enrichment Score", 
+#           y = NULL, 
+#           colour = "-log<sub>10</sub>(FDR)<br>(Benjamini-Hochberg)",
+#           size = "Number of Genes"
+#         ) +
+#         theme_minimal(base_size = 10, base_family = "Tex Gyre Termes") +
+#         theme(
+#           plot.title.position = "plot",
+#           plot.caption.position = "plot",
+#           plot.title = element_markdown(),
+#           plot.subtitle = element_markdown(face = "italic", size = rel(0.80)),
+#           plot.caption = element_markdown(face = "italic", size = rel(0.75)),
+#           axis.title.x = element_markdown(),
+#           axis.text.x = element_markdown(),
+#           axis.title.y = element_markdown(),
+#           axis.text.y = element_markdown(size = rel(0.75)),
+#           panel.grid.minor = element_blank(),
+#           egend.title = element_markdown()
+#         ) +
+#         guides(
+#           colour = guide_colourbar(
+#             title.position = "top", 
+#             title.hjust = 0.5, 
+#             barwidth = unit(10, units = "char"), 
+#             direction = "horizontal", 
+#             order = 2
+#           ),
+#           size = guide_legend(
+#             title.position = "top", 
+#             title.hjust = 0.5, 
+#             direction = "horizontal", 
+#             order = 1
+#           )
+#         )
+#     })
+#     
+#     caption_plots <- sapply(.enrich_sets, FUN = function(.enrich) {
+#       sprintf(
+#         fmt = "%s (p-value < %s)", 
+#         format(length(.enrich@geneList), digits = 0, big.mark = ","), 
+#         pvalue_gene
+#       )
+#     })
+#     caption_plots <- sprintf(fmt = "<b>%s</b>) %s",
+#       LETTERS[seq_along(caption_plots)],
+#       caption_plots
+#     )
+#     
+#     subtitle_plots <- sprintf(fmt = "<b>%s</b>) %s",
+#       LETTERS[seq_along(enrich_plots)], 
+#       names(enrich_plots)
+#     )
+#     
+#     agg_png(
+#       filename = file.path(output_directory, "gene_set_enrichment.png"),
+#       width = 16, height = 16, units = "cm", res = 300, scaling = 0.60
+#     )
+#       print(
+#         wrap_plots(c(enrich_plots, list(guide_area())), ncol = 2, guides = "collect") +
+#           plot_annotation(
+#             title = "Gene Set Enrichment Analysis",
+#             subtitle = sprintf("With %s and %s.",
+#               paste(subtitle_plots[-length(subtitle_plots)], collapse = ", "),
+#               subtitle_plots[length(subtitle_plots)]
+#             ),
+#             caption = sprintf("Mapped genes for %s and %s.",
+#               paste(caption_plots[-length(caption_plots)], collapse = ", "),
+#               caption_plots[length(caption_plots)]
+#             ),
+#             tag_levels = "A",
+#             theme = theme_minimal(base_size = 10, base_family = "Tex Gyre Termes") +
+#               theme(
+#                 plot.title.position = "plot",
+#                 plot.caption.position = "plot",
+#                 plot.title = element_markdown(),
+#                 plot.subtitle = element_markdown(face = "italic", size = rel(0.80)),
+#                 plot.caption = element_markdown(face = "italic", size = rel(0.75)),
+#                 axis.title.x = element_markdown(),
+#                 axis.text.x = element_markdown(),
+#                 axis.title.y = element_markdown(),
+#                 axis.text.y = element_markdown(),
+#                 panel.grid.minor = element_blank()
+#               )
+#           )
+#       )
+#     invisible(dev.off())
+#   }
+# ))
 
 
 ### Archive ========================================================================================
